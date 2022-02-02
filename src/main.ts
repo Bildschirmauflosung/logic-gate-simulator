51897f990b4366db5f458cd42050ffc65290b411
dyzio6684 <51453773+dyzio6684@users.noreply.github.com>
2022-02-02 21:30:50 +0100

    Move resizing canvas to function

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,13 @@ const nav: HTMLElement = document.querySelector(".navbar")!;
 const cv : HTMLCanvasElement = document.querySelector("#canvas")!;
 const ctx : CanvasRenderingContext2D = cv.getContext("2d")!;
 
-cv.width = document.body.clientWidth;
-cv.height = window.innerHeight - nav.offsetHeight;
-ctx.fillStyle = "#000";
-ctx.fillRect(0, 0, cv.width, cv.height);
-
-window.addEventListener("resize", () => {
+function resizeCanvas() {
   cv.width = window.innerWidth;
   cv.height = window.innerHeight - nav.offsetHeight;
   ctx.fillStyle = "#000";
   ctx.fillRect(0, 0, cv.width, cv.height);
-});
+}
+
+resizeCanvas();
+
+window.addEventListener("resize", () => resizeCanvas());
