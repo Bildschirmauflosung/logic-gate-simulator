1da0a3f565bbb1ef4212a9c666a786551980953d
dyzio6684 <51453773+dyzio6684@users.noreply.github.com>
2022-03-03 11:25:31 +0100

    Round connected wires

--- src/render/ConnectionData.ts
+++ src/render/ConnectionData.ts
@@ -7,19 +7,27 @@ export class ConnectionData {
   render(ctx: CanvasRenderingContext2D): void {
     ctx.beginPath();
     ctx.strokeStyle = Theme.fgColour;
+    const dr = this.pointFrom.top < this.pointTo.top ? 4 : this.pointFrom.top > this.pointTo.top ? -4 : 0;
     if (this.pointFrom.left + 16 >= this.pointTo.left) {
-      const hy = Math.round(Math.max(this.pointFrom.top, this.pointTo.top) + Math.min(this.pointFrom.top, this.pointTo.top)) / 2;
+      const hy = Math.round((this.pointTo.top + this.pointFrom.top) / 2);
       ctx.moveTo(this.pointFrom.left, this.pointFrom.top);
-      ctx.lineTo(this.pointFrom.left + 16, this.pointFrom.top);
-      ctx.lineTo(this.pointFrom.left + 16, hy);
-      ctx.lineTo(this.pointTo.left - 16, hy);
-      ctx.lineTo(this.pointTo.left - 16, this.pointTo.top);
+      ctx.lineTo(this.pointFrom.left + 12, this.pointFrom.top);
+      ctx.arcTo(this.pointFrom.left + 16, this.pointFrom.top, this.pointFrom.left + 16, this.pointFrom.top + dr, 4);
+      ctx.lineTo(this.pointFrom.left + 16, hy - dr);
+      ctx.arcTo(this.pointFrom.left + 16, hy, this.pointFrom.left + 12, hy, 4);
+      ctx.lineTo(this.pointTo.left - 12, hy);
+      ctx.arcTo(this.pointTo.left - 16, hy, this.pointTo.left - 16, hy + dr, 4);
+      ctx.lineTo(this.pointTo.left - 16, this.pointTo.top - dr);
+      ctx.arcTo(this.pointTo.left - 16, this.pointTo.top, this.pointTo.left - 12, this.pointTo.top, 4);
       ctx.lineTo(this.pointTo.left, this.pointTo.top);
       ctx.stroke();
     } else {
+      const hx = Math.round((this.pointTo.left + this.pointFrom.left) / 2);
       ctx.moveTo(this.pointFrom.left, this.pointFrom.top);
-      ctx.lineTo(Math.round((this.pointTo.left + this.pointFrom.left) / 2), this.pointFrom.top);
-      ctx.lineTo(Math.round((this.pointTo.left + this.pointFrom.left) / 2), this.pointTo.top);
+      ctx.lineTo(hx - 4, this.pointFrom.top);
+      ctx.arcTo(hx, this.pointFrom.top, hx, this.pointFrom.top + dr, 4);
+      ctx.lineTo(hx, this.pointTo.top - dr);
+      ctx.arcTo(hx, this.pointTo.top, hx + 4, this.pointTo.top, 4);
       ctx.lineTo(this.pointTo.left, this.pointTo.top);
       ctx.stroke();
     }
